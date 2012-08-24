955ce2506ef814682e5bae2a2e842b816eecd983
Apirada <apiradas@hotmail.com>
2012-08-24 18:22:26 +0700

    message for first commit

---
+++ app/models/sub.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby1.9
+
+class Class
+  #def subclasses
+    #ObjectSpace.each_object(Class).select { |klass| klass.superclass ==  self }
+      #.sort {|a,b| a.name <=> b.name }
+  #end
+  
+  def self.descendants
+    ObjectSpace.each_object(Class).select { |klass| klass.klass < self }
+  end
+  
+class Child < Parent
+  
+end  
+  
+class GrandChild < Child
+  
+end  
+
+puts Parent.descendants
+puts Child.descendants
+  
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test2::Application.config.secret_token = '96c012cfbbb8110522dc7815f61849267593739d29099647c266c609ef85c8a2fb15745eaf37c67122c34cd00b90aac389e10235dee8f51b41e77df1e402a7bb'
